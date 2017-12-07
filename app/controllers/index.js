fbb3c9ddb65f2c07d19c82e10f5646768a8c7e1f
Caio Costa <caio.costa93@gmail.com>
2017-12-07 10:14:43 -0200

    Computed properties and obeservers

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,27 @@
-import Ember from 'ember';
+import Controller from '@ember/controller';
 
-export default Ember.Controller.extend({
+export default Controller.extend({
 
-  isDisabled: true
+  isDisabled: true,
+
+  emailAddress: '',
+  
+    actualEmailAddress: Ember.computed('emailAddress', function() {
+      console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
+    }),
+  
+    emailAddressChanged: Ember.observer('emailAddress', function() {
+      console.log('observer is called', this.get('emailAddress'));
+    }),
+
+  nome: '',
+
+    actualNome: Ember.computed('nome', function(){
+      console.log('actualName function is called: ', this.get('name'));
+    }),
+
+    nomeChanged: Ember.observer('nome', function(){
+      console.log('observer is called', this.get('nome'));
+    })
 
 });
