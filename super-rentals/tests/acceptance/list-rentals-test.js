2099d28e83fa1557c39ab60d5375fdaa2c75c98d
Caio Costa <caio@nfe.io>
2018-02-02 18:07:42 +0000

    adjust acceptance tests

--- super-rentals/tests/acceptance/list-rentals-test.js
+++ super-rentals/tests/acceptance/list-rentals-test.js
@@ -4,12 +4,27 @@ import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptan
 moduleForAcceptance('Acceptance | list rentals');
 
 test('should show rentals as the home page', function (assert) {
+    visit('/');
+    andThen(() =>{
+        assert.equal(currentURL(), '/rentals', 'Should redirect automatically');
+    });
+
 });
 
 test('should link to information about the company.', function (assert) {
+    visit('/');
+    click('a:contains("About")');
+    andThen(() =>{
+        assert.equal(currentURL(), '/about', 'Should navigate to about');
+    });
 });
 
 test('should link to contact information.', function (assert) {
+    visit('/');
+    click('a:contains("Contact")');
+    andThen(() =>{
+        assert.equal(currentURL(), '/contact', 'Should navigate to contact')
+    });
 });
 
 test('should list available rentals.', function (assert) {
